813e4ad2d06dd627ce5ab6170af0e611571295d4
arbiter487 <arbiters487@gmail.com>
2019-10-18 16:34:48 -0400

    Updates

---
+++ precache-manifest.1055f7dbf9c27b9bf7c4a7ce65f144d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbe16d0b3ebc579fba81c3ad0917a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb9e656eacfb8a7259a0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "70dc495c5ad602ce427a",
+    "url": "/static/js/2.c97fde68.chunk.js"
+  },
+  {
+    "revision": "eb9e656eacfb8a7259a0",
+    "url": "/static/js/main.022f5cdb.chunk.js"
+  },
+  {
+    "revision": "355730ac88183be28ff1",
+    "url": "/static/js/runtime-main.0d8fc38d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
