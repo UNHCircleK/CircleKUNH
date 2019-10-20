c4e8293d9ed895dd27fde96ff1e13d86374b5460
Tomas Sandstrom <ts@rlsand.com>
2019-10-20 17:15:44 -0400

    testImagesAdded

--- src/Pages/ServiceCalendar.js
+++ src/Pages/ServiceCalendar.js
@@ -8,7 +8,7 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 class ServiceCalendar extends React.Component {
     render() {
         return <PageBox>
-            <p>click to sign up for listed events!</p>
+            <p>click to sign up for listed events! (not implemented yet)</p>
             <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
             </PageBox>
     }
